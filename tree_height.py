8f48e537e9c080fde969082f609e695e2eda49f6
Arvids221RDB160 <124888434+Arvids221RDB160@users.noreply.github.com>
2023-03-03 16:09:22 +0000

    tests14

--- tree_height.py
+++ tree_height.py
@@ -1,57 +1,66 @@
-#text = input()
-#if text == "I":
-    #n = int(input())
-#else:
+import os
+
+# Define a function to read input from the keyboard or a file
+def read_input():
+    # Ask the user whether they want to input from the keyboard or a file
+    source = input("Enter 'I' to input from keyboard or 'F' to input from a file: ")
+    
+    if source.upper() == 'I':
+        # Read input from the keyboard
+        n = int(input().strip())
+        parents = list(map(int, input().strip().split()))
+    elif source.upper() == 'F':
+        # Read input from a file
+        filename = input("Enter the file name (without the .txt extension): ")
+        # Check that the file name doesn't contain the letter 'a'
+        if 'a' in filename.lower():
+            print("File name contains the letter 'a'. Please choose a different name.")
+            return None
+        try:
+            with open(os.path.join('folder', f"{filename}.txt"), 'r') as f:
+                n = int(f.readline().strip())
+                parents = list(map(int, f.readline().strip().split()))
+        except FileNotFoundError:
+            print("File not found. Please check the file name and try again.")
+            return None
+    else:
+        # Invalid input
+        print("Invalid input. Please enter 'K' or 'F' to choose input source.")
+        return None
+    
+    # Check that the input is valid
+    if len(parents) != n:
+        print("Invalid input. Number of parent nodes does not match the number of nodes.")
+        return None
+    
+    return n, parents
 
 
-try:
-    num=""
-    n = input()
-    try:
-        n=int(n)
-        for x in range (0, n):
-            num = num+input()+" "
-    except ValueError:
-        n = int(input())
-        num = input()
-
-    parents = list(map(int, num.split()))
-    root=0
-    tree = [[] for _ in range(n)]
-    for child, parent in enumerate(parents):
-        if parent == -1:
-            root = child
+# Define a function to compute the height of the tree
+def compute_tree_height(n, parents):
+    # Create a dictionary to store the children of each node
+    children = {i: [] for i in range(n)}
+    for i in range(n):
+        if parents[i] != -1:
+            children[parents[i]].append(i)
+    
+    # Define a recursive function to compute the height of a node
+    def compute_height(node):
+        if not children[node]:
+            # Leaf node
+            return 0
         else:
-            tree[parent].append(child)
+            # Compute the height of each child and take the maximum
+            return 1 + max(compute_height(child) for child in children[node])
+    
+    # Compute the height of the root node
+    return compute_height(parents.index(-1))
 
-    stack = [(root, 0)]  # (node, depth)
-    depths = [0] * n
-    while stack:
-        node, depth = stack.pop()
-        depths[node] = depth
-        for child in tree[node]:
-            stack.append((child, depth+1))
-    height = max(depths)
-    print(height+1)
 
-except EOFError:
-    parents = list(map(int, num.split()))
-    root=0
-    tree = [[] for _ in range(n)]
-    for child, parent in enumerate(parents):
-        if parent == 1:
-            root = child
-        else:
-            tree[parent].append(child)
-            root=root+1
-
-    stack = [(root, 0)]  # (node, depth)
-    depths = [0] * n
-    while stack:
-        node, depth = stack.pop()
-        depths[node] = depth
-        for child in tree[node]:
-            stack.append((child, depth+1))
-    height = max(depths)
-    print(height+1)
+# Main program
+input_data = read_input()
+if input_data is not None:
+    n, parents = input_data
+    tree_height = compute_tree_height(n, parents)
+    print(tree_height+1)
     
