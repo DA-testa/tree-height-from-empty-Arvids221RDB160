e61011cd82c99e36e1ff19b7685dfac3aac4ea2c
Arvids221RDB160 <124888434+Arvids221RDB160@users.noreply.github.com>
2023-03-03 16:27:33 +0000

    tests19

--- tree_height.py
+++ tree_height.py
@@ -24,6 +24,7 @@ def read_input():
             print("File not found. Please check the file name and try again.")
             return None
     else:
+        print(source)
         n = int(input().strip())
         parents = list(map(int, input().strip().split()))
     
